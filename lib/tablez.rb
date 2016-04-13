e60c5e5896143b98a272ddb4d92ba7461ebc965c
Nick Palaniuk <npalaniuk@gmail.com>
2016-04-13 16:41:10 -0400

    Get title box rendering

--- lib/tablez.rb
+++ lib/tablez.rb
@@ -1,5 +1,70 @@
-require "tablez/version"
+class Settings
+  attr_accessor :title
 
-module Tablez
-  # Your code goes here...
+  def title_size
+    title.size
+  end
+
+  def total_padding_width
+    1 * 2
+  end
+
+  def max_width
+    [title_size].max + total_padding_width
+  end
+end
+
+module Display
+  def padding
+    " " * 1
+  end
+
+  def corner
+    "+"
+  end
+
+  def line_break
+    "\n"
+  end
+
+  def side_pipe
+    "|"
+  end
+end
+
+class Tablez
+  include Display
+
+  attr_reader :settings
+
+  def initialize(&block)
+    @settings = Settings.new
+    yield(@settings)
+  end
+
+  def title
+    @settings.title
+  end
+
+  def width
+    @settings.max_width
+  end
+
+  def separator
+    corner + ("-" * width) + corner + line_break
+  end
+
+  def separator_minus_line_break
+    corner + ("-" * width) + corner
+  end
+  alias_method :table_bottom, :separator_minus_line_break
+
+  def render_title
+    side_pipe + padding + title + padding + side_pipe + line_break
+  end
+
+  def render
+    separator + render_title + table_bottom
+  end
+
 end
--- spec/tablez_spec.rb
+++ spec/tablez_spec.rb
@@ -1,11 +1,54 @@
-require 'spec_helper'
+require "spec_helper"
 
-describe Tablez do
-  it 'has a version number' do
-    expect(Tablez::VERSION).not_to be nil
-  end
+RSpec.describe Tablez do
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe "Tablez" do
+    it "can set a title" do
+      table = Tablez.new do |t|
+                t.title = "This is a title"
+              end
+      expect(table.render).to eq <<-EOS.deindent
+      +-----------------+
+      | This is a title |
+      +-----------------+
+      EOS
+    end
+
+    # it "can set a title and empty rows" do
+    #   table = Tablez.new do |t|
+    #             t.title = "This is a title"
+    #             t.rows  = 2
+    #           end
+    #   expect(table.render).to eq <<-EOS.deindent
+    #   +-----------------+
+    #   | Here is a title |
+    #   +-----------------+
+    #   |                 |
+    #   +-----------------+
+    #   |                 |
+    #   +-----------------+
+    #   EOS
+    # end
+
+    # it "can add a row" do
+    #   table << [1, 'One']
+    #   expect(table.render).to eq <<-EOS.deindent
+    #   +---+-----+
+    #   | 1 | One |
+    #   +---+-----+
+    #   EOS
+    # end
+
+    # it "can add two rows at once" do
+    #   table << [[1, 'One'], [2, 'Two']]
+    #   expect(table.to_s).to eq <<-EOS.deindent
+    #   +---+-----+
+    #   | 1 | One |
+    #   +---+-----+
+    #   | 2 | Two |
+    #   +---+-----+
+    #   EOS
+    # end
+
   end
 end
